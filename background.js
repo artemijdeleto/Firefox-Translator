4ca8f52a00ef5339f38f3544d9082adb4ba552db
Artemij <artemijdeleto@gmail.com>
2019-12-25 02:20:05 +0400

    Switch between translation engines

--- background.js
+++ background.js
@@ -6,20 +6,24 @@ browser.contextMenus.create({
 
 browser.contextMenus.create({
 	id: 'newtab',
-	title: 'Open Google Translate'
+	title: 'Open translator'
 });
 
-browser.contextMenus.onClicked.addListener(function (info)
-{
-	switch (info.menuItemId)
-	{
+browser.contextMenus.onClicked.addListener(async info => {
+	switch (info.menuItemId) {
 		case 'translate':
 			translate(info.selectionText);
 			break;
 
 		case 'newtab':
+			let engine = (await browser.storage.local.get()).engine;
+			let url = 'https://translate.google.com/';
+			if (engine === 'yandex') {
+				url = 'https://translate.yandex.ru/';
+			}
+
 			browser.tabs.create({
-				url: 'https://translate.google.com/'
+				url: url
 			});
 			break;
 	}
@@ -27,14 +31,18 @@ browser.contextMenus.onClicked.addListener(function (info)
 
 browser.runtime.onMessage.addListener(translate);
 
-async function translate(text)
-{
-	if (text.length > 1)
-	{
+async function translate(text) {
+	if (text.length > 1) {
 		let lang = (await browser.storage.local.get()).lang;
+		lang = (lang !== '' && lang !== undefined) ? lang : 'en';
+		const engine = (await browser.storage.local.get()).engine;
+		let url = 'https://translate.google.com/#auto/' + lang  + '/' + encodeURIComponent(text);
+		if (engine === 'yandex') {
+			url = 'https://translate.yandex.ru/?lang=auto-' + lang + '&text=' + encodeURIComponent(text);
+		}
 
 		browser.tabs.create({
-			url: 'https://translate.google.com/#auto/' + ((lang !== '' && lang !== undefined) ? lang : 'en') + '/' + encodeURIComponent(text)
+			url: url
 		});
 	}
 }
--- options/options.js
+++ options/options.js
@@ -1,11 +1,14 @@
 (async () => {
 	const lang = (await browser.storage.local.get()).lang;
+	const engine = (await browser.storage.local.get()).engine;
 	document.getElementById('lang').value = lang ? lang : '';
+	document.getElementById('engine').value = engine ? engine : 'google';
 })();
 
 document.getElementById('form').addEventListener('submit', async e => {
 	e.preventDefault();
 	browser.storage.local.set({
-		lang: document.getElementById('lang').value
+		lang: document.getElementById('lang').value,
+		engine: document.getElementById('engine').value
 	});
 });
